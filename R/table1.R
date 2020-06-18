7667a78fc84067566002fcadf8447845f55f0b0b
Tangzer <perennec.tanguy@gmail.com>
2020-06-18 17:10:06 +0200

    Message

--- R/table1.R
+++ R/table1.R
@@ -11,7 +11,7 @@
 #' @import stringr
 #' @import stats
 #' @examples
-table1 <- function(DF,y,ynames=NULL,overall=TRUE,mutation=40){
+table1 <- function(DF,y,ynames=NULL,overall=TRUE,mutation=40,legend=FALSE,title=FALSE){
    # first transformations
    DF <- as.data.frame(DF)
    DF[,y] <- as.factor(DF[[y]])
@@ -22,6 +22,7 @@ table1 <- function(DF,y,ynames=NULL,overall=TRUE,mutation=40){
    if(!is.vector(ynames)) stop("ynames should be a vector of characters")
    if(length(ynames) != length(levels(DF[,y]))) stop("ynames should be of as many labels than y levels")
    if(!is.logical(overall)) stop("'overall' should be a booleen")
+   if(!is.logical(legend)) stop("'legend' should be a booleen")
    if(!is.numeric(mutation)) stop("'mutation' should be numeric")
 
 
@@ -37,6 +38,7 @@ table1 <- function(DF,y,ynames=NULL,overall=TRUE,mutation=40){
       i <- i + 1
       varname <- colnames(DF)[i]
       ligne <- varname
+      sign <- NULL
 
       if (is.numeric(var)){
          mean_vars <- aggregate(var~DF[,y],FUN="mean",na.rm=T)
@@ -56,18 +58,16 @@ table1 <- function(DF,y,ynames=NULL,overall=TRUE,mutation=40){
          }else{clig <- NA}
 
          ligne <- c(ligne,p)
-         tabf <- rbind(tabf,ligne) #ajout de la ligne au tableau
-      }else{
+         tabf <- rbind(tabf,ligne)
+      }else{#if non numeric
          var <- stringr::str_to_lower(var)
          var <- stringr::str_replace(var, "é", "e")
          var <- stringr::str_replace(var, "è", "e")
          var <- stringr::str_replace(var, "/", "")
          for (it in var){
             if(!is.na(it)){
-            if(stringr::str_length(it) > 20){
-               var[var == it] <- paste0(substr(it,1,20),"...")
-            }
-            }
+            if(stringr::str_length(it) > 40) var[var == it] <- paste0(substr(it,1,40),"...")
+            }else{}
          }
          var <- as.factor(var)
          if(length(levels(var)) >= mutation){
@@ -80,20 +80,37 @@ table1 <- function(DF,y,ynames=NULL,overall=TRUE,mutation=40){
             as.factor(var) -> var
          }
          if(length(levels(var)) >= 2) {
-         tb <- table(DF[,y],var) # on crée un tableau croisé
-         tbm <- margin.table(tb,1) # Sommes par ligne
+         tb <- table(DF[,y],var)
+         tbm <- margin.table(tb,1)
          verif_level <- margin.table(table(var,DF[,y]),2)
          verif <- TRUE
          for(lev in verif_level){
             if(lev == 0) verif <- FALSE
          }
          if(verif){
-         clig <- chisq.test(var,DF[,y])$p.value  # Calcul du Chi2
-         clig <- signif(clig,3)
-         }else{clig <- NA}
+            condition_chi2 <- 0
+            for(m in tb){if(m < 5){#counting modalities under 5. if one > 3 and only 2 column we can apply yate's correction otherwise Fisher
+               if(m < 3){
+                  condition_chi2_B <- FALSE
+                  break
+               }else{condition_chi2 <- condition_chi2 + 1}
+            }
+            condition_chi2_B <- ifelse(condition_chi2 < 2,TRUE,FALSE)
+            if(condition_chi2_B){
+            clig <- chisq.test(var,DF[,y])$p.value                    # Chi2 test
+            clig <- signif(clig,3)
+            }else{
+            clig <- fisher.test(var,DF[,y],simulate.p.value=TRUE)$p.value
+            clig <- signif(clig,3)
+            sign <- "‡"
+            }
+            }
+            }else{clig <- NA}
 
-         if (length(levels(var)) == 2){   # Variables à 2 niveaux
-            if (levels(var)[1] == "non"){var <- relevel(var,"oui")}
+
+         ## Variable with 2 levels
+         if (length(levels(var)) == 2){
+            if (levels(var)[1] == "non" || levels(var)[1] == "no"){var <- ifelse(levels(var)[1] == "no",relevel(var,"yes"),relevel(var,"oui"))}
             ligne <- paste0(ligne, " (",levels(var)[1],") - no. (%)")
 
             for (j in 1:levels_y){
@@ -101,10 +118,10 @@ table1 <- function(DF,y,ynames=NULL,overall=TRUE,mutation=40){
                pn <- signif(100*no/tbm[j],3)
                ligne <- c(ligne,paste0(no," (",pn,"%)"))
             }
-            ligne <- c(ligne,clig)
+            ligne <- c(ligne,paste0(clig,sign))
             tabf <- rbind(tabf,ligne) #ajout de la ligne au tableau
          }else{
-            ligne <- c(paste0(varname," - no. (%)"),rep(" ",levels_y),clig)
+            ligne <- c(paste0(varname," - no. (%)"),rep(" ",levels_y),clig,paste0(clig,sign))
             tabf <- rbind(tabf,ligne)
             for (n in 1:length(levels(var))){ #Pour chaque niveau de la variable
                ligne <- paste0("        ",levels(var)[n])
@@ -121,5 +138,18 @@ table1 <- function(DF,y,ynames=NULL,overall=TRUE,mutation=40){
       }
    }
    row.names(tabf) <- NULL
+
+   if(legend){
+      text_legend <- "p-values are obtained with a Chi2 or Fisher exact test (‡) for categorical variables and by student test for continuous variables"
+   }else{text_legend <- NULL}
+
+   if(title){
+      title_text <- "Table 1. Patients baseline characteristics by study group"
+   }else{title_text <- NULL}
+
+
+
    return(tabf)
+
 }
+
